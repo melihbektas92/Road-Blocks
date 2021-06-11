5538392c202fe735e7efa7366b53ce282e8291d4
Melih Bektaş <melihbektas92@gmail.com>
2021-06-11 18:02:27 +0300

    Seting Player movement, Camera movement and Obstacle trigger

---
+++ Road Blokcs/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sideWaysForce = 500f;
+
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideWaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideWaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
